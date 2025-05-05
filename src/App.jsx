23e5aa2a753371c8b543003622de19c88acd7a4a
gpinula-2020433 <gpinula-2020433@kinal.edu.gt>
2025-05-05 00:11:47 -0600

    Se importa Toaster en App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import { routes } from './routes'
 import { useRoutes } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 export const App = () => {
   const elements = useRoutes(routes)
   return (
     <>
       {elements}
+      <Toaster position="bottom-right" reverseOrder={false}/>
     </>
   )
 }
